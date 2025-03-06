95707b31072f0f66eb6bbdc27d59a38e01e74dcf
DulajB <160257859+DulajB@users.noreply.github.com>
2025-03-06 15:24:11 +0530

    added grade validation

--- models/StudentRecord.js
+++ models/StudentRecord.js
@@ -3,7 +3,7 @@ const mongoose = require("mongoose");
 
 const studentRecordSchema = new mongoose.Schema({
   studentID: { type: String, required: true, unique: true },
-  grade: { type: Number, required: true },
+  grade: { type: Number, required: true, enum: [1,2,3,4,5] },
   class: { type: String, required: true },
   language: { type: String },
   religion: { type: String },
