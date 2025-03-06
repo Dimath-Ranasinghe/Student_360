0a0103ad4ddedcc7e3f757f2566804dcad444837
DulajB <160257859+DulajB@users.noreply.github.com>
2025-03-06 15:52:57 +0530

    Added validation to select language and religion

--- models/StudentRecord.js
+++ models/StudentRecord.js
@@ -5,8 +5,8 @@ const studentRecordSchema = new mongoose.Schema({
   studentID: { type: String, required: true, unique: true },
   grade: { type: Number, required: true, enum: [1,2,3,4,5] },
   class: { type: String, required: true, match:/^[A-Z]$/ },
-  language: { type: String },
-  religion: { type: String },
+  language: { type: String, enum:["Sinhala", "Tamil"], required:true },
+  religion: { type: String, enum:["Buddhism", "Hinduism", "Islam", "Christianity"], required:true },
   terms: [
     {
       term: { type: String, required: true },
