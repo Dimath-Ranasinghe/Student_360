a1c2edee07812082a259e35de1a523ab04f74e6d
DulajB <160257859+DulajB@users.noreply.github.com>
2025-03-06 13:34:36 +0530

    Created StudentRecord.js

---
+++ models/StudentRecord.js
@@ -0,0 +1,25 @@
+
+const mongoose = require("mongoose");
+
+const studentRecordSchema = new mongoose.Schema({
+  studentID: { type: String, required: true, unique: true },
+  grade: { type: Number, required: true },
+  class: { type: String, required: true },
+  language: { type: String },
+  religion: { type: String },
+  terms: [
+    {
+      term: { type: String, required: true },
+      subjects: [
+        {
+          subjectName: { type: String, required: true },
+          marks: { type: Number },
+        },
+      ],
+    },
+  ],
+});
+
+const StudentRecord = mongoose.model("StudentRecord", studentRecordSchema);
+module.exports = StudentRecord;
+
