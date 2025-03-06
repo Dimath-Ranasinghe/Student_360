496082347f911584cb324c4bd17307b5b5354d3d
DulajB <160257859+DulajB@users.noreply.github.com>
2025-03-06 15:27:39 +0530

    Added class validation(A-Z)

--- models/StudentRecord.js
+++ models/StudentRecord.js
@@ -4,7 +4,7 @@ const mongoose = require("mongoose");
 const studentRecordSchema = new mongoose.Schema({
   studentID: { type: String, required: true, unique: true },
   grade: { type: Number, required: true, enum: [1,2,3,4,5] },
-  class: { type: String, required: true },
+  class: { type: String, required: true, match:/^[A-Z]$/ },
   language: { type: String },
   religion: { type: String },
   terms: [
