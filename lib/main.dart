8a420fb5d93d16dea05c99ee617ab7908b60afef
Benul Fernando <161554872+Benulfernando@users.noreply.github.com>
2025-03-06 09:01:08 +0530

    Created the form page and updated the main method

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,7 @@
 import 'dart:async';
 
 import 'package:flutter/material.dart';
+import 'package:student360/Pages/form_page.dart';
 
 
 
@@ -16,7 +17,7 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
-      home: LoginScreen(),
+      home: FormPage(),
     );
   }
 }
