9bbd3e25a6c13bcc4999a19815b1dfedda6aa91e
Yumira _Chanul <127539584+yumira-chanul@users.noreply.github.com>
2025-03-09 03:19:55 +0530

    Add Express JSON parsing middleware

--- Backend/notice-board/server.js
+++ Backend/notice-board/server.js
@@ -2,6 +2,9 @@ const express = require('express');
 const app = express();
 const port = 3000;
 
+// Middleware
+app.use(express.json());
+
 app.listen(port, () => {
   console.log(`Server running on port ${port}`);
 });
