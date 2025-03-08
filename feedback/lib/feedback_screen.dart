42639fea9d9503dca416d543fec0fddc3d0e8b60
dimathcr <dimathcr@gmail.com>
2025-03-09 00:47:15 +0530

    Setup main and feedback screen

---
+++ feedback/lib/feedback_screen.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class FeedbackScreen extends StatelessWidget {
+  const FeedbackScreen({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text("Feedback Section"),
+      ),
+      body: Center(child: Text('Feedback Screen')),
+    );
+  }
+}
