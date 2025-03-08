164097ac08338a5cf4bbf9bd181a5b4571febf4d
dimathcr <dimathcr@gmail.com>
2025-03-09 01:20:53 +0530

    Add search bar to select the receiver

--- feedback/lib/feedback_screen.dart
+++ feedback/lib/feedback_screen.dart
@@ -4,17 +4,40 @@ class FeedbackScreen extends StatelessWidget {
   const FeedbackScreen({Key? key}) : super(key: key);
 
   @override
+  _FeedbackScreenState createState() => _FeedbackScreenState();
+}
+ class _FeedbackScreenState extends State<FeedbackScreen> {
+    final TextEditingController _controller = TextEditingController();
+  @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
         title: const Text("Feedback Section"),
       ),
-      body: Center(child: Text('Feedback Screen')),
-       color: Colors.white,
-       child:Center(
-            child:Text('Feedback Screen Body',)
-        )
-       )
+      backgroundColor: Colors.white,
+       body:Column(
+        children: [
+            const SizedBox(height: 16.0),
+            Padding(
+                paddding: const EdgeInsets.all(16.0),//the gap below the app bar
+                child: TextField(
+                    controller: _searchController,
+                    decoration: InputDecoration(
+                        hintText: 'Select student ID/Teacher',
+                        prefixIcon:const Icon(Icons.search),
+                        border: OutlineInputBorder(),
+                            borderRadius: BorderRadius.circular(8.0),
+                        ),
+                    ) 
+                ),
+            ),
+            Expanded(child: Container()),
+        ],
+    ),      
+    
+       
+              
+       
     );
   }
 }
