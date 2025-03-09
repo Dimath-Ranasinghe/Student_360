d281706aa7baccdabc2ab32ce916fc63503587cf
Tiyana04 <tiyanaludowyke04@gmail.com>
2025-03-09 15:18:12 +0530

    Add POST route for teacher profile creation

---
+++ routes/teacherProfileRoutes.js
@@ -0,0 +1,5 @@
+const express = require('express');
+const router = express.Router();
+const teacherProfileController = require('../controllers/teacherProfileController');
+
+router.post('/', teacherProfileController.createProfile);
