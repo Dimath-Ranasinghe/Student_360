8aa28710a6e91f7eb439a2292a3101e82bf1ae76
Yumira _Chanul <127539584+yumira-chanul@users.noreply.github.com>
2025-03-09 18:21:59 +0530

    Add validation to check if title and content exist in POST request

--- Backend/notice-board/server.js
+++ Backend/notice-board/server.js
@@ -5,6 +5,10 @@ const port = 3000;
 
 // Middleware
 app.use(express.json());
+app.use((req, res, next) => {
+  console.log(`${req.method} ${req.url}`);
+  next();
+});
 
 // MongoDB Connection
 const db = require('./db');
