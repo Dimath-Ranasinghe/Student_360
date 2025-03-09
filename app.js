c9eb857d65f5c1fc1eaee5cdc3f66ed63bf1ff31
Tiyana04 <tiyanaludowyke04@gmail.com>
2025-03-09 11:21:48 +0530

    Connect app to MongoDB in app.js

---
+++ app.js
@@ -0,0 +1,17 @@
+require('dotenv').config();
+const express = require('express');
+const connectDB = require('./config/db');
+const teacherProfileRoutes = require('./routes/teacherProfileRoutes');
+
+const app = express();
+
+connectDB();
+
+app.use(express.json());
+
+app.use('/api/teachers', teacherProfileRoutes);
+
+const PORT = process.env.PORT || 5000;
+app.listen(PORT, () => {
+    console.log(`Server running on port ${PORT}`);
+});
