0bb3208650ec177ef61ba5e8deaa513179a510bc
Yumira _Chanul <127539584+yumira-chanul@users.noreply.github.com>
2025-03-09 10:15:53 +0530

    Update Notice schema to add validation

--- Backend/notice-board/models/noticeModel.js
+++ Backend/notice-board/models/noticeModel.js
@@ -1,8 +1,8 @@
 const mongoose = require('mongoose');
 
 const noticeSchema = new mongoose.Schema({
-  title: { type: String, required: true },
-  content: { type: String, required: true },
+  title: { type: String, required: [true, 'Title is required'] },
+  content: { type: String, required: [true, 'Content is required'] },
   date: { type: Date, default: Date.now },
 });
 
