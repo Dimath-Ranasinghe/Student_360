a6ddc568a7c290a3d614ed9784232b8d8c0330d4
dimathcr <dimathcr@gmail.com>
2025-03-09 21:45:16 +0530

    Fixed issues and aligments in sever.js

--- Backend/server.js
+++ Backend/server.js
@@ -1,9 +1,14 @@
 const express = require('express');
+const mongoose = requires ('mongoose');
+const http = require('http');
+const socketIo = require('socket.io');
 const messagesRouter = require('./routees/messages');
 const usersRouter = require('./routes/users');
+
 const app = express();
+
 app.use(express.json());
-const PORT = process.env.PORT || 3000;
+
 
 app.get('/', (req, res) => {
   res.send('Feedback Feature Backend');
@@ -11,20 +16,20 @@ app.get('/', (req, res) => {
 
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
 //connect to mongodb
-const mongoose = requires ('mongoose');
+
 const mongoURI = process.env.MONGO_URI ||'mongodb://localhost:27017/feedbackApp';
 mongoose.connect(mongoURI, {useNewUrlParser: true, useUnifiedTopology: true})
 .then(() => console.log('Connected to MongoDB'))
 .catch(err => console.error('MongoDB connection error:', err));
 
 
-//Socket.io for real time communication
 
-const http = require('http');
-const socketIo = require('socket.io');
 
+
+//HTTP server
 const server = http.createServer(app);
-const io = socketIo(server, {cors:{origin:"*",meathods:["GET","POST"]}});
+const io = socketIo(server, {
+    cors:{origin:"*",meathods:["GET","POST"]}});
 //API
 app.use('/api/messages', messagesRouter);
 app.use('/api/users', usersRouter);
@@ -32,6 +37,7 @@ app.use('/api/users', usersRouter);
 //Socket.io  for real time communication
 io.on('connection', (socket) => {
     console.log('New client connected');
+
     socket.on('sendMessage',(data)=>{
         io.emit('receiveMessage',data);//mew message
     });
@@ -39,6 +45,6 @@ io.on('connection', (socket) => {
         console.log('Client disconnected');
     });
 });
-
+const PORT = process.env.PORT || 3000;
 //server
 server.listen(PORT, () => console.log(`Server running on port ${PORT}`));
