04c41724115375a1a45eaadb4034d644817b11a9
Yumira _Chanul <127539584+yumira-chanul@users.noreply.github.com>
2025-03-09 10:12:14 +0530

    Link the routes to the server in server.js

--- Backend/notice-board/server.js
+++ Backend/notice-board/server.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const app = express();
+const noticeRoutes = require('./routes/noticeRoutes');
 const port = 3000;
 
 // Middleware
@@ -14,6 +15,8 @@ mongoose.connect('mongodb://localhost:27017/student', {
 .then(() => console.log('MongoDB connected'))
 .catch(err => console.error(err));
 
+// Routes
+app.use('/api/notices', noticeRoutes);
 
 app.listen(port, () => {
   console.log(`Server running on port ${port}`);
