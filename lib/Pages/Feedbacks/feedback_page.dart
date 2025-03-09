7df90dc89a82843c5ca3acfdc0898aa72f676c85
Benul Fernando <161554872+Benulfernando@users.noreply.github.com>
2025-03-10 00:03:13 +0530

    Added the dummy feedback page

---
+++ lib/Pages/Feedbacks/feedback_page.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/material.dart';
+
+class FeedbackPage extends StatefulWidget {
+  const FeedbackPage({super.key});
+
+  @override
+  State<FeedbackPage> createState() => _FeedbackPageState();
+}
+
+class _FeedbackPageState extends State<FeedbackPage> {
+  @override
+  Widget build(BuildContext context) {
+    return const Scaffold(
+      backgroundColor: Color.fromRGBO(67, 67, 67, 1),
+    );
+  }
+}
