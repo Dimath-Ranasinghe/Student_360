47fda640508511d7e67bf9e28042aa1e49a3e08c
dimathcr <dimathcr@gmail.com>
2025-03-09 19:06:31 +0530

    connect with mongoDB

--- Backend/server.js
+++ Backend/server.js
@@ -8,3 +8,9 @@ app.get('/', (req, res) => {
 });
 
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
+//connect to mongodb
+const mongoose = requires ('mongoose');
+const mongoURI = process.env.MONGO_URI ||'mongodb://localhost:27017/feedbackApp';
+mongoose.connect(mongoURI, {useNewUrlParser: true, useUnifiedTopology: true})
+.then(() => console.log('Connected to MongoDB'))
+.catch(err => console.error('MongoDB connection error:', err));
