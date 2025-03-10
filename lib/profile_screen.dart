db9945aa8c789f19263b7e2102d140f2159a26fe
Rushith Sathnuka <ruahith11@gmail.com>
2025-03-10 14:49:12 +0530

    create header

--- lib/profile_screen.dart
+++ lib/profile_screen.dart
@@ -1,42 +1,51 @@
 import 'package:flutter/material.dart';
 
 class ProfileScreen extends StatelessWidget {
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      backgroundColor: Color(0xFF1E1E1E), // Dark background color
-      appBar: AppBar(
-        centerTitle: true,
+@override
+Widget build(BuildContext context) {
+return Scaffold(
+backgroundColor: Color(0xFF1E1E1E), // Dark background color
+appBar: AppBar(
+title: Row(
+mainAxisAlignment: MainAxisAlignment.center,
+children: [
 
-      ),
+SizedBox(width: 8),
+Text('STUDENT 360°'),
+],
+),
+centerTitle: true,
 
-    );
-  }
+),
+
+);
+}
 }
 
 // Widget for displaying profile details
 class ProfileDetail extends StatelessWidget {
-  final String title;
-  final String value;
+final String title;
+final String value;
 
-  ProfileDetail({required this.title, required this.value});
+ProfileDetail({required this.title, required this.value});
 
-  @override
-  Widget build(BuildContext context) {
-    return Padding(
-      padding: const EdgeInsets.symmetric(vertical: 4),
-      child: Row(
-        crossAxisAlignment: CrossAxisAlignment.start,
-        children: [
+@override
+Widget build(BuildContext context) {
+return Padding(
+padding: const EdgeInsets.symmetric(vertical: 4),
+child: Row(
+crossAxisAlignment: CrossAxisAlignment.start,
+children: [
 
-          Expanded(
-            child: Text(
-              value,
-              style: TextStyle(fontWeight: FontWeight.bold),
-            ),
-          ),
-        ],
-      ),
-    );
-  }
+Expanded(
+child: Text(
+value,
+style: TextStyle(fontWeight: FontWeight.bold),
+),
+),
+],
+),
+);
 }
+}
+
