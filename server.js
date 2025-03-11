8aef6c7174e65be59942f6c0fd630be790f39787
DulajB <160257859+DulajB@users.noreply.github.com>
2025-03-11 15:57:23 +0530

    Initialized database connection and HTTP server

--- server.js
+++ server.js
@@ -3,3 +3,6 @@ require("dotenv").config();
 const http = require("http");
 const app = require("./src/app");
 const connectDB = require("./src/config/db");
+
+connectDB();
+const server = http.createServer(app);
