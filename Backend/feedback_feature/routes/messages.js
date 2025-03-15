bcacecdd78af351e3297977e63dfeb9fdb5c663f
dimathcr <dimathcr@gmail.com>
2025-03-15 13:22:11 +0530

    updateed folder structure with cotroller folder

--- Backend/feedback_feature/routes/messages.js
+++ Backend/feedback_feature/routes/messages.js
@@ -1,35 +1,9 @@
 const express = require('express');
-const Message = require('../models/Message');
+const Message = require('../controllers/messagesController');
 
 const router = express.Router();
 
-// GET: Fetch messages between two users (using query parameters: from, to)
-router.get('/', async (req, res) => {
-  const { from, to } = req.query;
-  try {
-    const messages = await Message.find({
-      $or: [
-        { from, to },
-        { from: to, to: from }
-      ]
-    }).sort({ timestamp: 1 });
-    res.json(messages);
-  } catch (err) {
-    res.status(500).json({ message: err.message });
-  }
-});
-
-// POST: Create a new message
-router.post('/', async (req, res) => {
-    const { text, from, to } = req.body;
-    const message = new Message({ text, from, to });
-    try {
-      const savedMessage = await message.save();
-      res.status(201).json(savedMessage);
-    } catch (err) {
-      res.status(400).json({ message: err.message });
-    }
-  });
-  
+router.get('/', messagesController.getMessages);
+router.post('/', messagesController.createMessage);
 
 module.exports = router;
