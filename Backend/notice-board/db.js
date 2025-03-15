92bf78a8b4109909f80572d4dea64b195e6d88a8
Yumira _Chanul <127539584+yumira-chanul@users.noreply.github.com>
2025-03-15 22:01:13 +0530

    Changes

--- Backend/notice-board/db.js
+++ Backend/notice-board/db.js
@@ -1,5 +1,5 @@
 const mongoose = require('mongoose');
 module.exports = mongoose.connect('mongodb://localhost:27017/student', {
-  useNewUrlParser: true,
-  useUnifiedTopology: true,
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
 });
