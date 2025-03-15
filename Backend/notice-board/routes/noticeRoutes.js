a872c758268117b2c0534b6e54c5c78c01612fba
Yumira _Chanul <127539584+yumira-chanul@users.noreply.github.com>
2025-03-15 13:31:44 +0530

    Create controllers and modify routes

--- Backend/notice-board/routes/noticeRoutes.js
+++ Backend/notice-board/routes/noticeRoutes.js
@@ -1,49 +1,13 @@
 const express = require('express');
-const Notice = require('../models/noticeModel');
+const { createNotice, getNotices, deleteNotice } = require('../controllers/noticeController');
 
 const router = express.Router();
 
-// Create a new notice
-router.post('/', async (req, res) => {
-    try {
-      const { title, content } = req.body;
-      
-      // check if title or content is missing
-      if (!title || !content) {
-        return res.status(400).json({ error: 'Title and content are required' });
-    }
+// Routes
+router.post('/', createNotice);
+router.get('/', getNotices);
+router.delete('/:id', deleteNotice);
 
-      const notice = new Notice({ title, content });
-      await notice.save();
-      res.status(201).json({ message: 'Notice created successfully', notice });
-    } catch (error) {
-      res.status(500).json({ error: 'Unable to process request at this moment. Please try again later.' });
-    }
-  });
-
-// Get all notices  
-router.get('/', async (req, res) => {
-    try {
-      const notices = await Notice.find({}, '_id title content date').sort({ date: -1 });
-      res.status(200).json({ message: 'Notices fetched successfully', data: notices });
-    } catch (error) {
-      res.status(500).json({ error: 'Unable to process request at this moment. Please try again later.' });
-    }
-  });
-
-// Delete a notice by ID
-router.delete('/:id', async (req, res) => {
-    try {
-      const { id } = req.params;
-      const notice = await Notice.findByIdAndDelete(id);
-      if (!notice) {
-        return res.status(404).json({ error: 'Notice not found' });
-      }
-      res.status(200).json({ message: 'Notice deleted successfully' });
-    } catch (error) {
-      res.status(500).json({ error: 'Unable to process request at this moment. Please try again later.' });
-    }
-});
-  
+module.exports = router;
 
 module.exports = router;
