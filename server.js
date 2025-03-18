0672b0c94d493e5d4b1d48dd0bddbac22531a8d1
DulajB <160257859+DulajB@users.noreply.github.com>
2025-03-18 23:49:38 +0530

    Added Socket.IO for real-time messaging

--- server.js
+++ server.js
@@ -3,8 +3,17 @@ require("dotenv").config();
 const http = require("http");
 const app = require("./src/app");
 const connectDB = require("./src/config/db");
+const socketHandler = require("./src/sockets/socketHandler");
+const socketIo = require("socket.io");
+
 
 connectDB();
 const server = http.createServer(app);
+
+const io = socketIo(server, {
+    cors: { origin: "*", methods: ["GET", "POST"] },
+});
+
+socketHandler(io);
 
 server.listen(process.env.PORT || 5000, () => console.log(`Server running`));
