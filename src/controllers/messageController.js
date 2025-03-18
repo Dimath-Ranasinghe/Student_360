7784d7b31fc1c0ca395edd4e0d0eea724dcbeee6
DulajB <160257859+DulajB@users.noreply.github.com>
2025-03-18 23:40:28 +0530

    Added message creation and retrieval functionality

---
+++ src/controllers/messageController.js
@@ -0,0 +1,27 @@
+const Message = require("../models/Message");
+
+exports.getMessages = async (req, res) => {
+  const { from, to } = req.query;
+  try {
+    const messages = await Message.find({
+      $or: [
+        { from, to },
+        { from: to, to: from }
+      ]
+    }).sort({ timestamp: 1 });
+    res.json(messages);
+  } catch (err) {
+    res.status(500).json({ message: err.message });
+  }
+};
+
+exports.createMessage = async (req, res) => {
+  const { text, from, to } = req.body;
+  const message = new Message({ text, from, to });
+  try {
+    const savedMessage = await message.save();
+    res.status(201).json(savedMessage);
+  } catch (err) {
+    res.status(400).json({ message: err.message });
+  }
+};
