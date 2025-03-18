38e9650d2d3b962396d99b611f732c399b5a7fcc
Benul Fernando <161554872+Benulfernando@users.noreply.github.com>
2025-03-18 19:25:02 +0530

    Reupdated the base class

--- lib/API/basedata.dart
+++ lib/API/basedata.dart
@@ -1,5 +1,9 @@
 class Base {
-  static String baseURL = 'http://10.0.2.2:3000';
-  static String getNotice = '$baseURL/api/notices';
-  static String postNotice = '$baseURL/api/notices';
+  static String baseURL = 'http://10.0.2.2:3001/api/notices';
+
+  static String getNotice = baseURL;
+  static String postNotice = baseURL;
+
+  static String deleteNotice(String noticeId) => "$baseURL/$noticeId";
 }
+
