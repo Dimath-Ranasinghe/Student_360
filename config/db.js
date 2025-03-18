8b20c78fca1a39638a7540e32aa6f4a6c6b741e7
Tiyana04 <tiyanaludowyke04@gmail.com>
2025-03-18 09:18:25 +0530

    Create db.js

--- config/db.js
+++ config/db.js
@@ -2,13 +2,16 @@ const mongoose = require('mongoose');
 
 const connectDB = async () => {
     try {
-        const conn = await mongoose.connect(process.env.MONGO_URI, {
-            useNewUrlParser: true,
-            useUnifiedTopology: true,
-        });
+        // Check if MongoDB URI is defined in environment variables
+        if (!process.env.MONGODB_URI) {
+            throw new Error('MongoDB URI is not defined in environment variables');
+        }
+        
+        const conn = await mongoose.connect(process.env.MONGODB_URI);
+        
         console.log(`MongoDB Connected: ${conn.connection.host}`);
     } catch (error) {
-        console.error(`Error: ${error.message}`);
+        console.error(`Error connecting to MongoDB: ${error.message}`);
         process.exit(1);
     }
 };
